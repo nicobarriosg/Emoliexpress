08ecd10c908946c442ea50d9136a5b5d8d761e92
nicobarriosg <barriosnicobarrios@gmail.com>
2025-09-27 14:10:17 -0300

    about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,31 @@
-export default function About() {
+import React from "react";
+import "../App.css";
+import nosotrosImg from "../img/about.avif"; // 👈 importa la imagen
+
+function About() {
   return (
-    <div className="about">
-      <h2>Sobre Nosotros</h2>
-      <p>
-        En <span>Emoli</span> llevamos más de 10 años ofreciendo insumos eléctricos
-        de alta calidad. Nuestra misión es entregar productos confiables y un servicio
-        cercano a cada cliente.
-      </p>
-    </div>
+    <section className="about">
+      <div className="about-content">
+        <div className="about-text">
+          <h2>Sobre Nosotros</h2>
+          <p>
+            Somos una empresa que nace para dar soluciones rápidas y efectivas
+            a sus necesidades. Con más de 36 años de experiencia en el mercado
+            eléctrico, Mario Molina Lagos, director general de EMOLIEXPRESS
+            emprende su propio negocio dedicado a la comercialización de una
+            amplia gama de materiales eléctricos en general.
+          </p>
+          <p>
+            Hoy podemos decir que somos una empresa emergente, gracias a la
+            confianza que han puesto en nosotros nuestros fieles clientes.
+          </p>
+        </div>
+        <div className="about-image">
+          <img src={nosotrosImg} alt="Sobre nosotros" />
+        </div>
+      </div>
+    </section>
   );
 }
+
+export default About;
